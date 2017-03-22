e0dadbddb8f5151985d81a3e70ac5ec5cc28f652
Michael King <kingmt@gmail.com>
2017-03-22 09:47:27 -0500

    add more tests

--- spec/lib/omega_spec.rb
+++ spec/lib/omega_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe Omega do
   end
 
   describe 'fetch' do
+    # VCR setup?
   end
 
   describe 'convert_to_pennies' do
@@ -17,10 +18,6 @@ RSpec.describe Omega do
       it "converts #{price} into #{pennies} pennies" do
         expect(Omega.convert_to_pennies price).to eq pennies
       end
-    end
-
-    it 'without the $' do
-      expect(Omega.convert_to_pennies '$56.78').to eq 5678
     end
   end
 
@@ -49,12 +46,28 @@ RSpec.describe Omega do
   end
 
   describe 'update' do
-    # debatable if this needs a test or not
-    # this method doesn't do any real work
-    # it delegates to a method to do work
-    # or fires a logger statement
-    # what could be tested is making sure
-    # that the methods are called
+    let!(:product) { Product.create external_product_id: 213,
+                                    product_name: 'nice chair',
+                                    price: 1999
+                    }
+
+    context 'name matches' do
+      expect(Omega).to receive(:update_product)
+      record = {'id' => 1234,
+                'name' => 'nice chair',
+                'price' => '$12.34'
+               }
+      Omega.update product, record
+    end
+
+    context 'name mismatch' do
+      record = {'id' => 1234,
+                'name' => 'ratty old chair',
+                'price' => '$12.34'
+               }
+      expect(Rails.logger).to receive(:error).with /Product has a name mismatch!/
+      Omega.update product, record
+    end
   end
 
   describe 'create_new_product' do
@@ -80,8 +93,13 @@ RSpec.describe Omega do
 
   describe 'process_record' do
     context 'product found' do
-      Product.create external_product_id: 213
       # verify that the next method in the chain is called
+      Product.create external_product_id: 213,
+                     product_name: 'nice chair',
+                     price: 1999
+      json = {'external_product_id' => 123}
+      expect(Omega).to receive(:update)
+      Omega.process_record json
     end
 
     context 'product not found' do
@@ -108,6 +126,16 @@ RSpec.describe Omega do
   end
 
   describe 'update_all' do
+    it 'stops when given invalid data' do
+      expect(Rails.logger).to receive(:error).with "Unrecognized data from Omega Pricing Inc"
+      Omega.update_all foo: :bar
+    end
+
+    it 'process records' do
+      json = {'productRecords' => [{"id": 123}]}
+      expect(Omega).to receive(:process_record)
+      Omega.update_all json
+    end
   end
 
 end
