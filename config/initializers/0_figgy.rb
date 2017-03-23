a631bfa513e358825f808a5129305e43f55cf148
Michael King <kingmt@gmail.com>
2017-03-22 19:19:52 -0500

    install and setup figgy

--- config/initializers/0_figgy.rb
+++ config/initializers/0_figgy.rb
@@ -4,9 +4,5 @@ Figs = Figgy.build do |config|
   # config.foo is read from etc/foo.yml
   config.define_overlay :default, nil
 
-   # config.foo is then updated with values from etc/development/foo.yml
-   #                                        then etc/staging/foo.yml
-   #                                        then etc/production/foo.yml
-   # up to the current Rails environment, last definition wins
-   config.define_overlay(:environment), ['development', 'staging', 'production'].slice(0..(env.index(Rails.env)))
- end
+  config.define_overlay(:environment) { Rails.env }
+end
